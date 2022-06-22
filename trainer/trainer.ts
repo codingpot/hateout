4f78de43ce86516ef3d072f7ff7d4ea60bd0a8f7
Mo Kweon <kkweon@gmail.com>
2022-06-22 21:45:15 +0000

    feat: add a simple NN trainer

---
+++ trainer/trainer.ts
@@ -0,0 +1,65 @@
+import * as use from "@tensorflow-models/universal-sentence-encoder";
+import * as tf from "@tensorflow/tfjs-node";
+
+async function main() {
+  const encoder = await use.load();
+  const data = tf.data
+    .csv(
+      "https://raw.githubusercontent.com/smilegate-ai/korean_unsmile_dataset/main/unsmile_train_v1.0.tsv",
+      {
+        delimiter: "\t",
+        hasHeader: true,
+        configuredColumnsOnly: true,
+        columnConfigs: {
+          clean: {
+            dtype: "int32",
+            isLabel: true,
+          },
+          문장: {
+            dtype: "string",
+          },
+        },
+      }
+    )
+    .mapAsync(async (data: any) => {
+      const out = await encoder.embed(data.xs["문장"]);
+      return {
+        xs: out.flatten(),
+        ys: Object.values(data.ys),
+      };
+    })
+    .batch(32)
+    .shuffle(32);
+
+  const model = tf.sequential({
+    layers: [
+      tf.layers.dense({
+        inputDim: 512,
+        units: 512,
+        activation: "relu",
+      }),
+      tf.layers.batchNormalization(),
+      tf.layers.dense({
+        units: 512,
+        activation: "relu",
+      }),
+      tf.layers.batchNormalization(),
+      tf.layers.dense({
+        units: 1,
+        activation: "sigmoid",
+      }),
+    ],
+  });
+
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.sigmoidCrossEntropy,
+    metrics: [tf.metrics.binaryAccuracy],
+  });
+
+  model.fitDataset(data, {
+    epochs: 5,
+  });
+}
+
+main();
