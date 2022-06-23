4a2db96bdb9468589bde799f26e9a4e0357fb940
Chansung Park <deep.diver.csp@gmail.com>
2022-06-23 23:48:28 +0000

    add validation dataset

--- trainer/trainer.ts
+++ trainer/trainer.ts
@@ -3,7 +3,7 @@ import * as tf from "@tensorflow/tfjs-node";
 
 async function main() {
   const encoder = await use.load();
-  const data = tf.data
+  const train_data = tf.data
     .csv(
       "https://raw.githubusercontent.com/smilegate-ai/korean_unsmile_dataset/main/unsmile_train_v1.0.tsv",
       {
@@ -32,6 +32,35 @@ async function main() {
     .batch(32)
     .shuffle(32);
 
+    const val_data = tf.data
+    .csv(
+      "https://raw.githubusercontent.com/smilegate-ai/korean_unsmile_dataset/main/unsmile_valid_v1.0.tsv",
+      {
+        delimiter: "\t",
+        hasHeader: true,
+        configuredColumnsOnly: true,
+        columnConfigs: {
+          clean: {
+            dtype: "int32",
+            isLabel: true,
+          },
+          문장: {
+            dtype: "string",
+          },
+        },
+      }
+    )
+    .filter((data: any) => data.xs["clean"] != "")
+    .mapAsync(async (data: any) => {
+      const out = await encoder.embed(data.xs["문장"]);
+      return {
+        xs: out.flatten(),
+        ys: Object.values(data.ys),
+      };
+    })
+    .batch(32)
+    .shuffle(32);    
+
   const model = tf.sequential({
     layers: [
       tf.layers.dense({
@@ -58,8 +87,9 @@ async function main() {
     metrics: [tf.metrics.binaryAccuracy],
   });
 
-  model.fitDataset(data, {
+  model.fitDataset(train_data, {
     epochs: 5,
+    validationData: val_data
   });
 }
 
